c30c847b8e565100fac9f34391c2d3e4abbbddf1
Joel Angel <joelbaraka4325@gmail.com>
2022-10-06 21:55:53 +0300

    gradeint added

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function NFTDropPage() {
-  return <div>This is the NFTDropPage</div>;
+  return (
+    <div className="flex h-screen flex-col">
+      {/* LEFT SIDE */}
+
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div>
+          <h1>HELLO</h1>
+        </div>
+      </div>
+      {/* Right Side */}
+
+      <div></div>
+    </div>
+  );
 }
 
 export default NFTDropPage;
