82551e968e4c26d00eb47430efde79749f510ca8
Joel Angel <joelbaraka4325@gmail.com>
2022-10-06 22:07:35 +0300

    styling the nft image

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -6,12 +6,20 @@ function NFTDropPage() {
       {/* LEFT SIDE */}
 
       <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
-        <div>
-          <img
-            className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
-            src="https://links.papareact.com/8sg"
-            alt=""
-          />
+        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div className="bg-gradient-to-br from-yellow-400 to-purple-600">
+            <img
+              className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+              src="https://links.papareact.com/8sg"
+              alt=""
+            />
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold text-white">Angels Apes</h1>
+            <h2 className="text-xl text-gray-300">
+              A collection of Joel Angel Apes who live and breath the future
+            </h2>
+          </div>
         </div>
       </div>
       {/* Right Side */}
