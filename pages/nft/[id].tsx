0590ab48a00fd931d4e4caa006f0c5bac7bccb4e
Joel Angel <joelbaraka4325@gmail.com>
2022-10-19 23:51:51 +0300

    done

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -12,10 +12,11 @@ function NFTDropPage() {
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* LEFT SIDE */}
+
       {/* Span Across 4 columns */}
       <div className="lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500">
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
-          <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600  p-2">
+          <div className="rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
             <img
               className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
               src="https://links.papareact.com/8sg"
